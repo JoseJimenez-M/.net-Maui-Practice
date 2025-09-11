a3fb9839fe14325c15e84f6fba78e4e7a88198a1
Smit0309 <katarasmit102@gmail.com>
2025-09-11 16:08:56 -0400

    Added quiz question pages

---
+++ QuizTestAndroidApp/QuizTestAndroidApp/Data/QuestionData.cs
@@ -0,0 +1,31 @@
+﻿using QuizTestAndroidApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizTestAndroidApp.Data
+{
+    internal class QuestionData
+    {
+
+        public static List<Question> Questions = new()
+        {
+            new Question
+            {
+                Text = "What is the capital of France?",
+                Options = new List<string> { "Berlin", "Madrid", "Paris" },
+                CorrectOptionIndex = 2
+            },
+            new Question
+            {
+                Text = "Which planet is known as the Red Planet?",
+                Options = new List<string> { "Venus", "Mars", "Jupiter" },
+                CorrectOptionIndex = 1
+            }
+        };
+    }
+
+}
+
---
+++ QuizTestAndroidApp/QuizTestAndroidApp/Question2Page.xaml.cs
@@ -0,0 +1,9 @@
+namespace QuizTestAndroidApp;
+
+public partial class Question2Page : ContentPage
+{
+	public Question2Page()
+	{
+		InitializeComponent();
+	}
+}
