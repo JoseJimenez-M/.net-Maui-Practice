e180a255f1748205206c7d5a090614c7cb1c602e
JoseJimenez-M <jimenez331375@gmail.com>
2025-09-12 11:52:01 -0400

    added Jose02

--- QuizTestAndroidApp/QuizTestAndroidApp/Jose02.xaml.cs
+++ QuizTestAndroidApp/QuizTestAndroidApp/Jose02.xaml.cs
@@ -6,4 +6,40 @@ public partial class Jose02 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Rigth(object sender, EventArgs e)
+    {
+        CorrectAnswer(sender, e);
+        await Navigation.PushAsync(new Sujal02());
+    }
+
+    private async void Wrong(object? sender, EventArgs e)
+    {
+        if (QuizScoreService.Score > 0)
+        {
+            WrongAnswer(sender, e);
+        }
+
+        if (sender is Button btn)
+        {
+            var originalColor = btn.BackgroundColor;
+            btn.BackgroundColor = Colors.Red;
+            await Task.Delay(1000);
+            btn.BackgroundColor = originalColor;
+        }
+    }
+
+    //SCORE
+    private void CorrectAnswer(object sender, EventArgs e)
+    {
+        QuizScoreService.AddPoint(1);
+        scoreLabel.Text = $"Score: {QuizScoreService.Score}";
+    }
+
+    private void WrongAnswer(object sender, EventArgs e)
+    {
+        QuizScoreService.AddPoint(-1);
+        scoreLabel.Text = $"Score: {QuizScoreService.Score}";
+    }
+
 }
